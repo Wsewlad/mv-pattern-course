fc09a45e6ca77fe11825e3df00f845fec9ac7f96
vfil <wladyslawfil@gmail.com>
2023-03-11 15:26:30 +0200

    Inject CoffeeModel as environmentObject

--- HelloCoffee/HelloCoffee/HelloCoffeeApp.swift
+++ HelloCoffee/HelloCoffee/HelloCoffeeApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct HelloCoffeeApp: App {
+    
+    @StateObject private var model: CoffeeModel
+    
+    init() {
+        let webservice = Webservice()
+        _model = StateObject(wrappedValue: CoffeeModel(webservice: webservice))
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
         }
     }
 }
